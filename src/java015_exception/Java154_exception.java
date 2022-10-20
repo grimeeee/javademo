72599ee08399f6ad6188a343533df67102229962
grimeeee <dpfla889@gmail.com>
2022-10-20 18:15:34 +0900

    ADD | 20221020

--- src/java015_exception/Java154_exception.java
+++ src/java015_exception/Java154_exception.java
@@ -5,19 +5,23 @@ public class Java154_exception {
 	public static void main(String[] args) {
 			int[] num = new int[3];
 			num[0] = 10;
-			System.out.println(num[0]);
-			
-			try {
+			System.out.println(num[0]);		
+			process(num);
+	}
+	
+	public static void display(int[] num) {
+		System.out.println(num[5]);
+	}
+	public static void process(int[] num) {
+		try {
 				//예외 발생 가능 구문
 			System.out.println(num[4]);
 			}catch(ArrayIndexOutOfBoundsException ex) {
 				//System.out.println(ex.getMessage());
 				//System.out.println(ex.toString());
 				//ex.printStackTrace();
-				
 			}finally {
 				System.out.println("program end");
 			}
 	}
-
 }
