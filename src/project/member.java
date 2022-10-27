09b9993880082d74a8051e27d9ef962ef0003cc5
grimeeee <dpfla889@gmail.com>
2022-10-27 17:30:49 +0900

    ADD | 20221027

--- src/project/member.java
+++ src/project/member.java
@@ -4,4 +4,81 @@ public class member {
 int mnum;
 String name;
 String phone;
+String membership;
+String pt;
+String locker;
+String sweatsuit;
+
+public int getMnum() {
+	return mnum;
+}
+
+public void setMnum(int mnum) {
+	this.mnum = mnum;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getPhone() {
+	return phone;
+}
+
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+public String getMembership() {
+	return membership;
+}
+
+public void setMembership(String membership) {
+	this.membership = membership;
+}
+
+public String getPt() {
+	return pt;
+}
+
+public void setPt(String pt) {
+	this.pt = pt;
+}
+
+public String getLocker() {
+	return locker;
+}
+
+public void setLocker(String locker) {
+	this.locker = locker;
+}
+
+public String getSweatsuit() {
+	return sweatsuit;
+}
+
+public void setSweatsuit(String sweatsuit) {
+	this.sweatsuit = sweatsuit;
+}
+
+public member(int mnum, String name, String phone, String membership, String pt, String sweatsuit, String locker ) {
+	super();
+	this.mnum = mnum;
+	this.name = name;
+	this.phone = phone;
+	this.membership = membership;
+	this.pt = pt;
+	this.sweatsuit = sweatsuit;
+	this.locker = locker;
+	
+}
+
+public member() {
+	
+}
+
 }
--- src/project/payment.java
+++ src/project/payment.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class payment {
-	private static ArrayList<Student> studentList = new ArrayList<>();
+	private static ArrayList<member> memberList = new ArrayList<>();
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -36,49 +36,160 @@ public class payment {
 		System.out.println("프로그램을 종료합니다.");	
 		break;
 		default:
-		System.out.println("잘못 입력하셨습니다. \n다시 입력해주세요.");
+		System.out.println("잘못 입력하셨습니다.\n다시 입력해주세요.");
 		break;
 		}
 		}
 		}
 	
 	private static void createMember() {
+		System.out.println("『 회원번호를 등록해주세요 』");
+		
+		member m = new member();
+	
+	
+		
+		
 		System.out.print("회원번호 : ");
 		int mnum = scanner.nextInt();
+		
+		boolean noneResult = false;
+		for (int i = 0; i < memberList.size(); i++) {
+		member mb = memberList.get(i);
+		if (String.valueOf(mb.getMnum()).equals(String.valueOf(mnum))) {
+		noneResult = true;
+		System.out.println("중복된 회원번호입니다.\n초기화면으로 돌아갑니다.");
+		return; 
+		}
+		}
+		
 		System.out.print("회원이름 : ");
 		String name = scanner.next();
 		System.out.print("연락처 : ");
 		String phone = scanner.next();
-	}
+		scanner.nextLine();
+
+	
+		m.setMnum(mnum);
+		m.setName(name);
+		m.setPhone(phone);
+		memberList.add(m);
+		System.out.println( name + "님 등록되었습니다.");
+		}
+	
 	
 	private static void modifyMember() {
-		// TODO Auto-generated method stub
+		System.out.println("『 수정할 회원의 회원번호를 입력해주세요. 』");
+		System.out.print("회원번호 : ");
+		int mnum = scanner.nextInt();
 		
-	}
+		boolean noneResult = false;
+		for (int i = 0; i < memberList.size(); i++) {
+		member m = memberList.get(i);
+		if (String.valueOf(m.getMnum()).equals(String.valueOf(mnum))) {
+		noneResult = true;
+		System.out.print("회원이름 : ");
+		String name = scanner.next();
+		System.out.print("연락처 : ");
+		String phone = scanner.next();
+		System.out.print("회원권: ");
+		String membership = scanner.next();
+		System.out.print("PT횟수: ");
+		String pt = scanner.next();
+		System.out.print("운동복(이용/미이용) : ");
+		String sweatsuit = scanner.next();
+		System.out.print("락커(이용/미이용) : ");
+		String locker = scanner.next();
+		scanner.nextLine();
+		
 	
-	private static void searchMember() {
-		// TODO Auto-generated method stub
+		m.setName(name);
+		m.setPhone(phone);
+		m.setMembership(membership);
+		m.setPt(pt);
+		m.setSweatsuit(sweatsuit);
+		m.setLocker(locker);
+		memberList.add(m);
+		System.out.println(name + "님의 정보가 수정되었습니다.");
+		break;
 		
-	}
+		}
+		}
+		if (!noneResult) {
+		System.out.println("검색결과가 없습니다.");
+		}
+		}
+		
+
+	private static void searchMember() {
+		System.out.println("『 검색할 회원의 회원번호를 입력해주세요. 』");
+		System.out.print("회원번호 : ");
+		int mnum = scanner.nextInt();
+		boolean noneResult = false;
+		for (int i = 0; i < memberList.size(); i++) {
+		member m = memberList.get(i);
+		if (String.valueOf(m.getMnum()).equals(String.valueOf(mnum))) {
+		noneResult = true;
+		System.out.println("회원번호: " + m.getMnum() + " | 이름: " + m.getName() + " | 연락처: " + m.getPhone() + " | 회원권: " + m.getMembership() + " | PT: "
+		+ m.getPt() + " | 운동복: " + m.getSweatsuit() + " | 락커: " + m.getLocker());
+		System.out.println();
+		System.out.println("검색결과입니다");
+		break;
+		}
+		}
+		if (!noneResult) {
+		System.out.println("검색결과가 없습니다.");
+		}
+		}
 	
 	private static void deleteMember() {
-		// TODO Auto-generated method stub
-		
-	}
+		System.out.println("『 삭제할 회원의 회원번호를 입력해주세요. 』");
+		System.out.print("회원번호 : ");
+		int mnum = scanner.nextInt();
+		boolean noneResult = false;
+		for (int i = 0; i < memberList.size(); i++) {
+		member m = memberList.get(i);
+		if (String.valueOf(m.getMnum()).equals(String.valueOf(mnum))) {
+		noneResult=true;
+		memberList.remove(i);
+		System.out.println(m.getName() + "님의 정보가 삭제되었습니다.");
+		break;
+		}
+		}
+		if (!noneResult) {
+		System.out.println("검색결과가 없습니다.");
+		}
+		}
 
 	private static void pay() {
-		// TODO Auto-generated method stub
+		System.out.print("회원번호 : ");
+		int mnum = scanner.nextInt();
+		boolean noneResult = false;
+		for (int i = 0; i < memberList.size(); i++) {
+		member m = memberList.get(i);
+		if (String.valueOf(m.getMnum()).equals(String.valueOf(mnum))) {
+		noneResult = true;
+		System.out.println("(1개월 / 3개월 / 6개월 / 12개월)");
+		System.out.print("회원권: ");
+		String membership = scanner.next();
+		System.out.print("PT횟수: ");
+		String pt = scanner.next();
+		System.out.print("운동복(이용/미이용) : ");
+		String sweatsuit = scanner.next();
+		System.out.print("락커(이용/미이용) : ");
+		String locker = scanner.next();
+		System.out.println("결제정보 등록 완료!");
+		m.setMembership(membership);
+		m.setPt(pt);
+		m.setSweatsuit(sweatsuit);
+		m.setLocker(locker);
 		
-	}
-
-	
-
-	
-
-	
-
-	
-
-	}
-
+		break;
+		}
+		}
+		if (!noneResult) {
+		System.out.println("검색결과가 없습니다.");
+		}
+		}
+		}
 
