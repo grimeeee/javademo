e079e5ec892ba93591382946edcc262ae7eb48f4
grimeeee <dpfla889@gmail.com>
2022-11-03 18:03:42 +0900

    ADD | 20221103

--- src/java023_jdbc/part02/DepartmentsDAO.java
+++ src/java023_jdbc/part02/DepartmentsDAO.java
@@ -10,7 +10,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
+ *  1. ClassNotFoundException 
+ *  해결) JRE System Library에서 ojdbc6.jar파일 확인
+ *      Class.forName(): 경로확인
+ *      
+ * 2. java.sql.SQLException: 
+ *     IO예외 상황:  The Network Adapter...
+ *                 Listener refused.....  
+ *                 
+ *     해결) 제어판->관리도구->서비스->OracleServiceXE,
+ *            OracleXETNSListener을 다시 시작한다.
  * 
+ * java.sql.PreparedStatement
+ * 1 반복되는 쿼리문의 수행에 사용한다.
+ * 2 미리 정의된 SQL문을 수행하기 때문에 Statement에 비해 속도가 빠르다.
+ * 3 위치표시자(placeholder)(?)를 사용하여 쿼리문을 간략하게 작성한다.
+ * 
+ * 싱글톤 패턴
+ *  : 하나의 객체만을 생성해서 사용할 수 있도록 설계한 구조이다.
+ *  1 생성자의 접근제어자는 private
+ *  2 객체자신을 생성을 한다.
+ *  3 생성된 객체를 넘겨줄 수 있는 메소드를 정의한다.
  */
 
 //DAO : Data Access Object(데이터 접근 객체)
